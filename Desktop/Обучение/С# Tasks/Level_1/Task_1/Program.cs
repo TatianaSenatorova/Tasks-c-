b103682c3d93b4b6d853cdd97e7c1b1b9a3ac8dd
unknown <1975tanya@bk.ru>
2022-08-25 00:17:02 +0300

    Task_2

---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Вывести квадрат числа*/
+
+/*int Square(int a)
+{
+ a *= a;
+ return a;
+}
+Console.Write("Enter a number: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write(Square(a));*/
+
+double Square(int a)
+{
+    double numSquare = Math.Pow(a, 2);
+    return numSquare;
+}
+Console.Write("Enter a number: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write(Square(a));
---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿/*По двум заданным числам проверять является ли первое квадратом второго*/
+
+void NumsSquare(int firstNum, int secondNum)
+{
+    if (secondNum * secondNum == firstNum)
+    Console.Write($"Yes, {secondNum} * {secondNum} = {firstNum}");
+    else Console.Write($"No, {secondNum} * {secondNum} != {firstNum}");
+}
+
+Console.Write("Enter a first number: ");
+int firstNum = int.Parse(Console.ReadLine());
+Console.Write("Enter a second number: ");
+int secondNum = int.Parse(Console.ReadLine());
+
+NumsSquare(firstNum, secondNum);
