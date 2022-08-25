c02cd22619762d7a4ff187084cd097044fb1f907
unknown <1975tanya@bk.ru>
2022-08-26 02:19:55 +0300

    Task_8

---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_7/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Показать числа от -N до N*/
+
+void rowNumbers(int number)
+{
+    if (number > 0)
+    for(int i = - number; i <= number; i++)
+    Console.Write($"{i},  ");
+    else for (int i = - number; i >= number; i--)
+    Console.Write($"{i},  ");
+  }
+
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+rowNumbers(number);
