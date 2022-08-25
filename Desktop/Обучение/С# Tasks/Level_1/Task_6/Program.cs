22bb89294bb9b0aef65c62e1105fb72e8e245d93
unknown <1975tanya@bk.ru>
2022-08-26 02:00:39 +0300

    Task_6

---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_6/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Выяснить является ли число чётным*/
+
+void EvenNum(int number)
+{
+    if (number % 2 == 0) Console.Write($"{number} is an even number");
+    else Console.Write($"{number} isn't an even number");
+}
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+EvenNum(number);
