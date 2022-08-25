e87c6875a5ebe4b63da51e91ea734e0953ccd8af
unknown <1975tanya@bk.ru>
2022-08-26 01:05:02 +0300

    Task_3

---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Даны два числа. Показать большее и меньшее число*/
+int MaxNum(int firstNum, int secondNum)
+{
+    if (firstNum > secondNum) return firstNum;
+    else return secondNum;
+}
+
+Console.Write("Enter a first number: ");
+int firstNum = int.Parse(Console.ReadLine());
+Console.Write("Enter a second number: ");
+int secondNum = int.Parse(Console.ReadLine());
+
+Console.Write($"Larger number → {MaxNum(firstNum, secondNum)}");
