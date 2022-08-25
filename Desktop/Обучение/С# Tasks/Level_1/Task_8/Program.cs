2163254eeefef21a6943d33558cac714ce4dd74a
unknown <1975tanya@bk.ru>
2022-08-26 02:27:14 +0300

    Task_8

---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_8/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Показать четные числа от 1 до N*/
+
+void EvenNumbers(int number)
+{
+    for (int i = 2; i <= number; i += 2)
+    Console.Write($"{i}, ");
+    
+}
+
+Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+EvenNumbers(number);
