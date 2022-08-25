1d02931e9fafe6eafb04532fe46f281b08fbf86d
unknown <1975tanya@bk.ru>
2022-08-26 01:40:22 +0300

    Task_5

---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_5/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Найти максимальное из трех чисел*/
+
+int MaxNum(int first, int second, int third)
+{
+    int max;
+    if(first >  second)  max = first;
+    else max = second;
+    if (max < third) max = third;
+    return max;
+}
+Console.Write("Enter a first number: ");
+int first = int.Parse(Console.ReadLine());
+Console.Write("Enter a second number: ");
+int second = int.Parse(Console.ReadLine());
+Console.Write("Enter a second number: ");
+int third = int.Parse(Console.ReadLine());
+
+
+Console.Write($"{MaxNum(first, second, third)} ");
