22cd5f14de24840f9b7cfad5b890b3a1c64fbe7c
unknown <1975tanya@bk.ru>
2022-08-27 00:40:11 +0300

    Task 11

---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_11/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Дано число из отрезка [10, 99]. Показать наибольшую цифру числа*/
+
+int MaxDigit(int number)
+{
+    int max;
+    if(number / 10 % 10 > number % 10)
+    max = number / 10 % 10;
+    else max = number % 10;
+    return max;
+}
+
+Console.WriteLine("Enter a two-digit number: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write($"Maximum digit in number is {MaxDigit(number)}");
