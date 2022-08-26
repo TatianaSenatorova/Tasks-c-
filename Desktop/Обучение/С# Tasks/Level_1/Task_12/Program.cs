34815bf73c4081d17ef248c7e89b0688a8d0fe2d
unknown <1975tanya@bk.ru>
2022-08-27 00:51:45 +0300

    Task 12

---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_12/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Удалить вторую цифру трёхзначного числа*/
+ int DelMidleDigit(int number)
+ {
+    int twoDigitNum = ((number / 100) % 10) * 10 + number % 10;
+    return twoDigitNum;
+ }
+
+ Console.WriteLine("Enter a three-digit number: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write($"Number without middle digit is {DelMidleDigit(number)}");
