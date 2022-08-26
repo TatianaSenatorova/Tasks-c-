0391c10a02b29404cb680088b65480ad3d81ae4b
unknown <1975tanya@bk.ru>
2022-08-27 00:11:21 +0300

    Task 9

---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_9/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Показать последнюю цифру трёхзначного числа*/
+
+int LastNumber(int number)
+{
+   number = number % 10;
+   return number;
+}
+Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write($"Last number is {LastNumber(number)}");
