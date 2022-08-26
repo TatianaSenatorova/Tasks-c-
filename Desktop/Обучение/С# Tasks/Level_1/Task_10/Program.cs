29a827862ee3302b18dc95f85d656b91490c5c1b
unknown <1975tanya@bk.ru>
2022-08-27 00:22:15 +0300

    Task 10

---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_10/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Показать вторую цифру трёхзначного числа*/
+
+int MiddleNum(int number)
+{
+    number /= 10;
+    number = number % 10;
+    return number;
+}
+
+Console.WriteLine("Enter a three-digit number: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write($"Middle number is {MiddleNum(number)}");
