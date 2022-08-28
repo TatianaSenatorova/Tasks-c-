fc0489eb4d4e81860d1d43eb9b2b7d215a663cae
unknown <1975tanya@bk.ru>
2022-08-29 00:57:42 +0300

    Task 13

---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_13/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Выяснить, кратно ли число заданному, если нет, вывести остаток*/
+
+void Multiple(int first_number, int second_number)
+{
+    int remainderDivision = 0;
+    if (first_number % second_number == 0) Console.Write($"{first_number} is multiple {second_number}");
+    else Console.Write($"{first_number} isn't multiple {second_number}. The remainder ot the division is {remainderDivision}"); 
+}
+
+Console.WriteLine("Enter first number: ");
+int first_number = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number (not zero): ");
+int second_number = int.Parse(Console.ReadLine());
+
+Multiple(first_number, second_number);
