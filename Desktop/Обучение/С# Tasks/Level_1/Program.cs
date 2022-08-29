c9cbc685aea9cc7875c756295fee72aaf6c685ec
unknown <1975tanya@bk.ru>
2022-08-29 23:49:17 +0300

    Task 14

---
+++ Desktop/Обучение/С# Tasks/Level_1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Найти третью цифру числа или сообщить, что её нет*/
+
+void ThirdDigit(int number)
+{
+    while (number % 1000 == 0)
+    {
+        number /=  10;
+    }
+    if (number % 100 == 0) Console.Write($"There isn't third digit in number");
+    else Console.Write($" {number % 100} third digit in number");
+}
+Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+ThirdDigit(number);
---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_14/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Найти третью цифру числа или сообщить, что её нет*/
+
+void ThirdDigit(int number)
+{
+    while(number / 1000 != 0)
+    {
+        number = number / 10;
+    }
+    if (number / 100 == 0) Console.Write($"There isn't third digit in number");
+    else Console.Write($"{number % 10} is third digit in number");
+}
+Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+ThirdDigit(number);
+
