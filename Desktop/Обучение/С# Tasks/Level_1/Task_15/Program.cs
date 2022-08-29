60b8002a623a693911fe5b8e721c4899802036ee
unknown <1975tanya@bk.ru>
2022-08-29 23:57:03 +0300

    Task 15

---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_15/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Дано число. Проверить кратно ли оно 7 и 23*/
+
+void Multiple(int number)
+{
+    if (number % 7 == 0 && number % 23 == 0)
+    Console.Write($"{number} is multiple to 7 and 23");
+    else Console.Write($"{number} isn't multiple 7 and 23");
+}
+Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+Multiple(number);
