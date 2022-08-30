87d6a05bd63586a7094842fa667d1568a207446e
unknown <1975tanya@bk.ru>
2022-08-31 00:06:19 +0300

    Task 16

---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_16/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Дано число обозначающее день недели. Выяснить является номер дня недели выходным */
+
+void WeekEnd(int numDay)
+{
+    if (numDay == 6 || numDay == 7)
+    Console.Write($"{numDay} is a weekend day");
+    else  if (numDay > 0 && numDay < 6) Console.Write($"{numDay} isn't a weekend day");
+    else Console.Write($"You enter wrong day. Try again");
+}
+Console.WriteLine("Enter a number of the week day: ");
+int numDay = int.Parse(Console.ReadLine());
+WeekEnd(numDay);
