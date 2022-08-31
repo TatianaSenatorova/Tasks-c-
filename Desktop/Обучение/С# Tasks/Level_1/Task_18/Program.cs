fe09d36558b3c8d2fd7810565e9bee5031b245f4
unknown <1975tanya@bk.ru>
2022-08-31 23:19:03 +0300

    Task 19

---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_18/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y*/
+
+void CheckLogic()
+{
+    bool X = true;
+    bool Y = true;
+    if (!(X || Y) == !X && !Y)
+    Console.Write($"true");
+    else Console.Write($"false");
+    
+}
+CheckLogic();
---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_19/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Определить номер четверти плоскости,
+в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0*/
+
+void QuaterCoordinate(int x, int y)
+{
+    if (x > 0 && y > 0)
+    Console.Write($"First quater of coordinates ");
+    else if (x < 0 && y > 0)
+    Console.Write($"Second quater of coordinates ");
+    else if (x < 0 && y < 0)
+    Console.Write($"Third quater of coordinates ");
+    else if (x > 0 && y < 0)
+    Console.Write($"Fourth quater of coordinates ");
+}
+Console.WriteLine("Enter X: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Y: ");
+int y = int.Parse(Console.ReadLine());
+
+QuaterCoordinate(x, y);
