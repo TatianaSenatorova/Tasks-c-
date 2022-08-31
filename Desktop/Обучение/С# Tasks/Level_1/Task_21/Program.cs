d7c62e541c1d77b78009747416847a2c9ba454d1
unknown <1975tanya@bk.ru>
2022-09-01 00:18:45 +0300

    Task 21

---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_21/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Программа проверяет пятизначное число на палиндромом.*/
+
+void Palindrome(int number)
+{
+    if (number / 10000 == number % 10 && (number / 10) % 10 == (number / 1000) % 10)
+        Console.Write($"yes, it's a palindrome");
+    else Console.Write($"no, it's not a palindrome");
+
+}
+
+Palindrome(12721);
