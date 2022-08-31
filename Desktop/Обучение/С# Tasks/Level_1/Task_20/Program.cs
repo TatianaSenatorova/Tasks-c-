948b7179dd171e26b8166c47e34250f158b7b33c
unknown <1975tanya@bk.ru>
2022-08-31 23:43:20 +0300

    Task 20

---
+++ Desktop/Обучение/С# Tasks/Level_1/Task_20/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задать номер четверти, показать диапазоны для возможных координат*/
+
+void Coordinates(int numQuaterCoordinates)
+{
+    if (numQuaterCoordinates == 1)
+    Console.Write($"x > 0 and y > 0");
+    else if (numQuaterCoordinates == 2)
+    Console.Write($"x < 0 and y > 0");
+    else if (numQuaterCoordinates == 3)
+    Console.Write($"x < 0 and y < 0");
+    else if (numQuaterCoordinates == 4)
+    Console.Write($"x > 0 and y < 0");
+    else Console.Write($"You write wrong number. Try again");
+
+}
+Console.WriteLine("Enter number of coordinates quater: ");
+int numQuaterCoordinates = int.Parse(Console.ReadLine());
+
+
+Coordinates(numQuaterCoordinates);
