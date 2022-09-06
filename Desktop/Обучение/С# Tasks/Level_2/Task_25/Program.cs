c2b19d55dfc376e80abc53f042854a5b3f029669
unknown <1975tanya@bk.ru>
2022-09-06 17:32:08 +0300

    Task 25

---
+++ Desktop/Обучение/С# Tasks/Level_2/Task_25/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Найти сумму чисел от 1 до А*/
+
+int SumNumbers(int A)
+{
+    int result = 0;
+    for (int i = 1; i <= A; i++)
+    result += i;
+    return result;
+}
+Console.WriteLine("Enter a number A: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.Write(SumNumbers(A));
