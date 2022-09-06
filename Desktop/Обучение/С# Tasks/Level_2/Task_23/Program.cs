c4b200efba7a2ffc9da91ced93c8ce8e00ac66f2
unknown <1975tanya@bk.ru>
2022-09-06 17:24:27 +0300

    Task 24

--- Desktop/Обучение/С# Tasks/Level_2/Task_23/Program.cs
+++ Desktop/Обучение/С# Tasks/Level_2/Task_23/Program.cs
@@ -2,7 +2,7 @@
 
 void SquareNumber(int number)
 {
-    for (int i = 0; i <= number; i++)
+    for (int i = 1; i <= number; i++)
     Console.Write($"{Math.Pow(i, 2)}, ");
 }
 
---
+++ Desktop/Обучение/С# Tasks/Level_2/Task_24/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Найти кубы чисел от 1 до N*/
+
+void CubNumber(int number)
+{
+    for (int i = 1; i <= number; i++)
+    Console.Write($"{Math.Pow(i, 3)}, ");
+}
+
+Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+
+CubNumber(number);
