0656cfd3c6f3888bfa51d7407506b7d627d87cf4
unknown <1975tanya@bk.ru>
2022-09-06 17:20:18 +0300

    Task 23

---
+++ Desktop/Обучение/С# Tasks/Level_2/Task_23/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Показать таблицу квадратов чисел от 1 до N */
+
+void SquareNumber(int number)
+{
+    for (int i = 0; i <= number; i++)
+    Console.Write($"{Math.Pow(i, 2)}, ");
+}
+
+Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+
+SquareNumber(number);
