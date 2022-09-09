909b3b24781a5cb827706ecd01aecdd66031c1b8
unknown <1975tanya@bk.ru>
2022-09-09 18:42:42 +0300

    task 26

---
+++ Desktop/Обучение/С# Tasks/Level_2/Task_26/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Возведите число А в натуральную степень B используя цикл*/
+
+int Exponentiation( int A, int B)
+{
+    int result = 1;
+    for (int i = 1; i <= B; i++)
+    result = result * A;
+    return result;
+}
+
+Console.WriteLine("Enter a number A: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter a degree raise: ");
+int B = int.Parse(Console.ReadLine());
+
+Console.Write(Exponentiation(A, B));
