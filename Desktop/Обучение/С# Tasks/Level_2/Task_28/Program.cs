62605ac7603f8c3f4d6b97437c82935985fc50f2
unknown <1975tanya@bk.ru>
2022-09-09 19:00:32 +0300

    task 28

---
+++ Desktop/Обучение/С# Tasks/Level_2/Task_28/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Подсчитать сумму цифр в числе*/
+
+int SumDigits(int number)
+{
+    int sum = 0;
+    while(number % 10 != 0)
+    {
+       sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write(SumDigits(number));
