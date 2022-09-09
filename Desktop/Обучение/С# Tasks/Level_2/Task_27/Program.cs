177d20c6d89ae8f10d43ab43a0e7891d77380a53
unknown <1975tanya@bk.ru>
2022-09-09 18:50:28 +0300

    task 27

---
+++ Desktop/Обучение/С# Tasks/Level_2/Task_27/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Определить количество цифр в числе*/
+
+int NumDigits(int number)
+{
+    int count = 1;
+    while (number / 10 != 0)
+    {
+        number /= 10;
+        count += 1;
+    }
+    return count;
+}
+
+Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write(NumDigits(number));
