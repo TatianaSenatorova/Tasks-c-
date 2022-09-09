559789521d610af5f36ba0d907b4932324df1039
unknown <1975tanya@bk.ru>
2022-09-09 19:07:56 +0300

    task 29

---
+++ Desktop/Обучение/С# Tasks/Level_2/Task_29/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Написать программу вычисления произведения чисел от 1 до N*/
+
+int Multiply(int number)
+{
+    int multiplyNums = 1;
+    for (int i = 2; i <= number; i++)
+    {
+        multiplyNums *= i;
+    }
+return multiplyNums;
+}
+
+Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write(Multiply(number));
