c5fa7f017ba44cf4a1aa15433cbeca3e9e84034d
unknown <1975tanya@bk.ru>
2022-09-11 23:54:36 +0300

    task 31

---
+++ Desktop/Обучение/С# Tasks/Level_3/Task_31/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задать массив из 8 элементов и вывести их на экран */
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    Console.Write($"{arr[i]}, ");
+}
+
+int [] FillMass(int count, int from, int to)
+{
+    Random r = new Random();
+    int[] arr = new int[count];
+    for (int i = 0; i < count; i++)
+    arr[i] = r.Next(from, to);
+    return arr;
+}
+
+Console.WriteLine("Enter a number of elements in mass: ");
+int count = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a low bound of a mass: ");
+int from = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter an upper bound of a mass: ");
+int to = int.Parse(Console.ReadLine());
+
+int [] arr_1 = FillMass(count, from, to);
+Print(arr_1);
