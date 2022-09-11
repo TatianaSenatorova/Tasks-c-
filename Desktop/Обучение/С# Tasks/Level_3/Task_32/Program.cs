818096dd12d56860a89f64a2ebcf81a00e7aa585
unknown <1975tanya@bk.ru>
2022-09-12 00:01:37 +0300

    task 32

---
+++ Desktop/Обучение/С# Tasks/Level_3/Task_32/Program.cs
@@ -0,0 +1,19 @@
+﻿ /*Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран*/
+
+ void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    Console.Write($"{arr[i]}, ");
+}
+
+int [] FillMass(int count, int from, int to)
+{
+    Random r = new Random();
+    int[] arr = new int[count];
+    for (int i = 0; i < count; i++)
+    arr[i] = r.Next(from, to);
+    return arr;
+}
+
+int [] arr_1 = FillMass(8, 0, 2);
+Print(arr_1);
