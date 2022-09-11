e73601aecc365b08d6a3b9420452c10de6469bba
unknown <1975tanya@bk.ru>
2022-09-11 23:19:19 +0300

    task 30

---
+++ Desktop/Обучение/С# Tasks/Level_2/Task_30/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Показать кубы чисел, заканчивающихся на четную цифру*/
+
+void CubsEndEven(int number)
+{
+    for (int i = 2; i <= number; i++)
+    if ((Math.Pow(i, 3)) % 2 == 0)
+    {
+        Console.WriteLine($"Cub of {i} is {Math.Pow(i, 3)} and ended on even digit");
+    }
+}
+
+Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+CubsEndEven(number);
