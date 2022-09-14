d9a4ea409624c54414fc94792fdb375240805c34
unknown <1975tanya@bk.ru>
2022-09-14 19:39:18 +0300

    tasks 35, 36

---
+++ Desktop/Обучение/С# Tasks/Level_3/Task_36/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Задать массив, заполнить случайными положительными трёхзначными числами.
+Показать количество нечетных\четных чисел*/
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr[i]}, ");
+    Console.WriteLine();
+}
+
+int[] FillMass(int count, int from, int to)
+{
+    Random r = new Random();
+    int[] arr = new int[count];
+    for (int i = 0; i < count; i++)
+        arr[i] = r.Next(from, to);
+    return arr;
+}
+
+int NumEvenElements(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            count += 1;
+    }
+    return count;
+}
+
+int NumUnEvenElements(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 != 0)
+            count += 1;
+    }
+    return count;
+}
+
+int[] arr_1 = FillMass(15, 100, 1000);
+Print(arr_1);
+Console.WriteLine($"Even numbers in mass: " + NumEvenElements(arr_1));
+Console.WriteLine($"Uneven numbers in mass: " + NumUnEvenElements(arr_1));
+
+
+
