6836d93a32c25e134e740f2a7826f61782550edc
unknown <1975tanya@bk.ru>
2022-09-14 19:23:46 +0300

    task 34

---
+++ Desktop/Обучение/С# Tasks/Level_3/Task_34/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Написать программу замену элементов массива на противоположные*/
+
+ void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    Console.Write($"{arr[i]}, ");
+    Console.WriteLine();
+}
+
+int [] FillMass(int count, int from, int to)
+{
+    Random r = new Random();
+    int[] arr = new int[count];
+    for (int i = 0; i < count; i++)
+    arr[i] = r.Next(from, to);
+    return arr;
+}
+
+int[] NewArr(int [] arr)
+{
+    int [] newArray = new int [arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    newArray[i] = -arr[i];
+    return newArray;
+}
+
+
+
+int [] arr_1 = FillMass(12, -9, 10);
+Print(arr_1);
+int [] newArr_1 = NewArr(arr_1);
+Print(newArr_1);
---
+++ Desktop/Обучение/С# Tasks/Level_3/Task_35/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Определить, присутствует ли в заданном массиве, некоторое число */
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr[i]}, ");
+    Console.WriteLine();
+}
+
+int[] FillMass(int count, int from, int to)
+{
+    Random r = new Random();
+    int[] arr = new int[count];
+    for (int i = 0; i < count; i++)
+        arr[i] = r.Next(from, to);
+    return arr;
+}
+
+void FindNum(int[] arr, int number)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == number)
+        {
+            Console.WriteLine($"Yes, the number {number} is present in mass");
+            break;
+        }
+        else if (arr[i] == arr[arr.Length - 1])
+            Console.WriteLine($"No, the number {number} is absent in mass");
+    }
+
+}
+
+Console.WriteLine("Enter any number: ");
+int number = int.Parse(Console.ReadLine());
+
+int[] arr_1 = FillMass(15, -9, 10);
+Print(arr_1);
+FindNum(arr_1, number);
+
+
