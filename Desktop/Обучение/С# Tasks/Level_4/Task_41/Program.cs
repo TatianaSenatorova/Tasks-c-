398bfff72dcad8a3cef29234c2ce8c4d52aaea96
unknown <1975tanya@bk.ru>
2022-09-15 21:32:49 +0300

    Task 41

---
+++ Desktop/Обучение/С# Tasks/Level_4/Task_41/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Выяснить являются ли три числа сторонами треугольника */
+
+void IfTriangle(int firstNumber, int secondNumber, int thirdNumber)
+{
+    if (firstNumber + secondNumber > thirdNumber && secondNumber + thirdNumber > firstNumber && firstNumber + thirdNumber > secondNumber)
+    Console.Write($"These numbers could be lengths of a triangle ");
+    else Console.Write($"These numbers couldn't be lengths of a triangle ");
+}
+
+Console.WriteLine("Enter the first number: ");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the second number: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the third number: ");
+int thirdNumber = int.Parse(Console.ReadLine());
+
+IfTriangle(firstNumber, secondNumber, thirdNumber);
