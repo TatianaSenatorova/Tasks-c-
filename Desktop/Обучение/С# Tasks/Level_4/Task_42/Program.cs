a5badc425909fd85c836948c47374c0ea44385dc
unknown <1975tanya@bk.ru>
2022-09-15 22:44:34 +0300

    Task 42

---
+++ Desktop/Обучение/С# Tasks/Level_4/Task_42/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Определить сколько чисел больше 0 введено с клавиатуры*/
+
+void PositiveNums(string [] numbers)
+{
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+       int num = int.Parse(numbers[i]);
+       if (num > 0)
+       count += 1;
+    }
+    Console.WriteLine($"Positive numbers in mass {count}");
+}
+
+Console.WriteLine("Enter several number divided by space: ");
+string s = Console.ReadLine();
+string[] numbers = s.Split(' ');
+PositiveNums(numbers);
+
