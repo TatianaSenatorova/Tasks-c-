118ca1ed5ee182b896aa09dd022742162ccf778a
unknown <1975tanya@bk.ru>
2022-09-16 18:21:56 +0300

    Task 44

---
+++ Desktop/Обучение/С# Tasks/Level_4/Task_43/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Написать программу преобразования десятичного числа в двоичное*/
+
+int FromDecimalToBinary(int number)
+{
+    string num = string.Empty;
+    while (number / 2 > 0)
+    {
+        int n = number % 2;
+        num = n + num;
+        number /= 2;
+    }
+    if (number % 2 > 0)
+    {
+        int n = number % 2;
+        num = n + num;
+    }
+
+    int binaryNum = int.Parse(num);
+    return binaryNum;
+}
+
+Console.WriteLine("Enter any decimal number: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write($"Number {number} in binery system " + FromDecimalToBinary(number));
