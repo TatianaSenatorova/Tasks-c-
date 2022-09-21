f62a17125daec2518c2b263493e00373051547bd
unknown <1975tanya@bk.ru>
2022-09-21 16:18:16 +0300

    Task 45

---
+++ Desktop/Обучение/С# Tasks/Level_4/Task_45/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Показать числа Фибоначчи*/
+
+void Fibonacci(int N)
+{
+    
+    int [] arr = new int [N];
+    arr[0] = 1;
+    arr[1] = 1;
+    Console.WriteLine($"F1 = {arr[0]}");
+    Console.WriteLine($"F2 = {arr[1]}");
+ for (int i = 2; i < arr.Length; i++)
+ {
+    arr[i] = arr[i - 1] + arr[i - 2];
+    Console.WriteLine($"F{i+1} = {arr[i]}");
+ }
+}
+
+Console.Write($"Enter number N for Fibonacci: ");
+int N = int.Parse(Console.ReadLine());
+
+Fibonacci(N);
